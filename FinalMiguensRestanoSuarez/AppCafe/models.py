ad029d0c5d621d8a5559d7382a0f838a8423a8d2
Alberto Restano <restanoalberto@gmail.com>
2022-03-26 15:37:19 -0300

    Vamos

---
+++ FinalMiguensRestanoSuarez/AppCafe/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class VariedadCafe(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    marca  = models.CharField(max_length=40)
+    precio = models.IntegerField()
+
+class VariedadTe(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    marca  = models.CharField(max_length=40)
+    precio = models.IntegerField()
+
+class VariedadGaseosa(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    marca  = models.CharField(max_length=40)
+    tipo   = models.CharField(max_length=40)
+    precio = models.IntegerField()
